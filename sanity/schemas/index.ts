844d476d81828443ff0f31d92e8792c6415e88e3
Rus Madrigal <rusbenmadrigal@gmail.com>
2026-03-19 21:20:30 -0600

    Update

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,14 +1,8 @@
 import { blockContent } from "./blockContent";
 import { caseStudy, caseStudyResult } from "./caseStudy";
-import { client } from "./client";
-import { monthlyReport } from "./monthlyReport";
-import { seoTask } from "./seoTask";
 
 export const schemaTypes = [
   blockContent,
   caseStudy,
   caseStudyResult,
-  client,
-  monthlyReport,
-  seoTask,
 ];
